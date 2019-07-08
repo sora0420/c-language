383f0a49ea0a316fed627f8869cd67adaadc91a7
sora0420 <thfk0420@naver.com>
2019-07-08 09:26:16 +0900

    Create star.c

---
+++ star.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main() {
+	int floor;
+	printf("¸î ÃþÀÇ º°À» ½×À»±î¿ä? : ");
+	scanf("%d", &floor);
+	for (int i = 0; i < floor; i++) {
+		for (int j = i; j < floor - 1; j++) {
+			printf(" ");
+		}
+		for (int k = 0; k < i * 2 + 1; k++) {
+			printf("*");
+		}
+		printf("\n");
+	}
+	return 0;
+
+}
